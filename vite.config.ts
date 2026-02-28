bbfff35c142f5b2d65a8e08f7440ed11cd73cdd1
Vladislav Vasilenko <mr.vasilenko.vlad@ya.ru>
2026-02-28 20:04:33 +0300

    Configure GitHub Pages deployment via GitHub Actions

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: '/', // Base path for GitHub Pages (vladislav-vasilenko.github.io)
+});
